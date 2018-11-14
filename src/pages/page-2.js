c5a225ffd1018d909f1e13e29ff6c7eee14082ae
Nirmal <nirmalkumarv@users.noreply.github.com>
2018-11-14 12:36:23 +0530

    Update page-2.js

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -5,8 +5,8 @@ import Layout from '../components/layout'
 
 const SecondPage = () => (
   <Layout>
-    <h1>Cart Page</h1>
-    <p>You don't have any items in your cart</p>
+    <h1>Newsletter Subscription Page</h1>
+    <p><a href="#" className="newsLetter">Click to Subscribe our Newsletter</a></p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
