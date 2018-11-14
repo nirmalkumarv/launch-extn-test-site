0e4876019d2a4ebdac283ebc0eb1dd5cb087ccaa
Nirmal <nirmalkumarv@users.noreply.github.com>
2018-11-14 12:37:48 +0530

    Create page-1.js

---
+++ src/pages/page-1.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const FirstPage = () => (
+  <Layout>
+    <h1>Product Store Page</h1>
+
+  </Layout>
+)
+
+export default FirstPage
